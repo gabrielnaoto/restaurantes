f804d8ec1fef1c330ee6a36ea120a2b580765816
Luma Hoscher <luhoscher@gmail.com>
2016-06-26 18:02:45 -0300

    10

--- software/restaurantes/src/java/br/udesc/restaurantes/bean/BeanCadastroUsuario.java
+++ software/restaurantes/src/java/br/udesc/restaurantes/bean/BeanCadastroUsuario.java
@@ -3,6 +3,11 @@ package br.udesc.restaurantes.bean;
 import br.udesc.restaurantes.modelo.dao.core.JPAFactory;
 import br.udesc.restaurantes.modelo.dao.core.UsuarioDAO;
 import br.udesc.restaurantes.modelo.entidade.Usuario;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -10,6 +15,9 @@ import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.ServletContext;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ViewScoped
@@ -17,13 +25,14 @@ public class BeanCadastroUsuario {
 
     private Usuario usuario;
     private UsuarioDAO dao;
+    private UploadedFile file;
 
     public BeanCadastroUsuario() {
         usuario = new Usuario();
         dao = JPAFactory.getUsuarioDAO();
     }
 
-    public String salvar() {
+    public String salvar() {       
         try {
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("RestaurantePU");
             EntityManager em = emf.createEntityManager();
@@ -41,7 +50,7 @@ public class BeanCadastroUsuario {
         if (dao.apelido(usuario.getApelido()) == null) {
             return salvar();
         } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Apelido ja existe"));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Apelido já existe"));
             return null;
         }
     }
@@ -62,4 +71,34 @@ public class BeanCadastroUsuario {
         this.dao = dao;
     }
 
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void criaArquivo(byte[] bytes, String arquivo) {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(arquivo);
+            fos.write(bytes);
+            fos.close();
+        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
+        }
+    }
+
+    public void upload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        byte[] foto = event.getFile().getContents();
+        String nomeArquivo = event.getFile().getFileName();
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
+        String arquivo = scontext.getRealPath("resources/img/" + nomeArquivo);
+        criaArquivo(foto, arquivo);
+        usuario.setFoto(arquivo);
+    }
 }
